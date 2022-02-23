493bd2624bb7c99ef4f2498b8493e1121c377604
Dymi.W <dymi.dev@gmail.com>
2022-02-24 00:19:52 +0100

    feat: 🎸 Init My Todo List project and add MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, OutlinedInput, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant='h3'align='center'>
+         My Todo List 
+      </Typography>;
+
+      <FormControl sx={{ width: '25ch' }}>
+        <OutlinedInput placeholder="Please enter text" />
+        
+      </FormControl>
     </div>
   );
 }
