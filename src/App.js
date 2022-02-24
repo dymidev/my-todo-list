666571bb6a9e76a76b296fa3095118ae084b092b
Dymi.W <dymi.dev@gmail.com>
2022-02-24 01:43:27 +0100

    feat: 🎸 AddTolist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import { FormControl, OutlinedInput, Typography, Box } from '@mui/material';
-
+import {
+  FormControl,
+  OutlinedInput,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
 function App() {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
+  console.log(list);
   return (
     <Box display="flex" justifyContent="center" flexDirection="column">
-      <Typography variant='h3'>
-         My Todo List 
-      </Typography>
+      <Typography variant="h3">My Todo List</Typography>
 
-      <FormControl sx={{ width: '25ch' }}>
-        <OutlinedInput placeholder="Please enter text" />
-        
+      <FormControl sx={{ width: "25ch" }} noValidate>
+        <OutlinedInput
+          placeholder="Please enter text"
+          value={todo}
+          onChange={(event) => {
+            setTodo(event.target.value);
+          }}
+        />
+        <Button onClick={addtolist} variant="contained">
+          Add
+        </Button>
       </FormControl>
     </Box>
   );
+
+  function addtolist() {
+    if (list[list.length - 1] === todo || todo === "") {
+      return;
+    }
+    setList([...list, todo]);
+    setTodo("");
+  }
 }
 
 export default App;
